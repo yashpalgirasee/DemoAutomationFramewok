0f8e318e4e7551bb7a44b0dcf0612681468268d6
Yashpal Girase <yashpalgirase10@gmail.com>
2023-07-31 13:22:31 +0530

    third commit

--- DemoAutomationFramework/src/test/java/mypackage/basetest/demo.java
+++ DemoAutomationFramework/src/test/java/mypackage/basetest/demo.java
@@ -5,7 +5,12 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Helo");
+		System.out.println("Hello");
+		
+		System.out.println("demo");
+		System.out.println("test");
+		System.out.println("user");
+		
 		
 	}
 
